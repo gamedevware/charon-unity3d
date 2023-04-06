43bf384eb8de74abe55ffdb354ca26023d3ce7ca
Denis Zykov <deniszykov@gmail.com>
2023-04-06 21:17:40 +0200

    chore: updated licensing information for used code

--- src/GameDevWare.Charon.Unity/Json/JsonValue.cs
+++ src/GameDevWare.Charon.Unity/Json/JsonValue.cs
@@ -1,4 +1,29 @@
-﻿using System;
+﻿/*
+ * Author @ Atsushi Enomoto  <atsushi@ximian.com>
+ * Copyright (c) 2001, 2002, 2003 Ximian, Inc and the individuals listed
+on the ChangeLog entries.
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
@@ -6,6 +31,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using JetBrains.Annotations;
+
 using JsonPair = System.Collections.Generic.KeyValuePair<string, GameDevWare.Charon.Unity.Json.JsonValue>;
 
 namespace GameDevWare.Charon.Unity.Json
@@ -527,7 +553,7 @@ namespace GameDevWare.Charon.Unity.Json
 			else if (value is IEnumerable)
 				// ReSharper disable RedundantTypeArgumentsOfMethod
 				jsonValue = new JsonArray(((IEnumerable)value).Cast<object>().Select<object, JsonValue>(From).ToArray());
-				// ReSharper restore RedundantTypeArgumentsOfMethod
+			// ReSharper restore RedundantTypeArgumentsOfMethod
 			else
 				jsonValue = JsonObject.From(value);
 
--- src/GameDevWare.Charon.Unity/Routines/ValidateGameDataRoutine.cs
+++ src/GameDevWare.Charon.Unity/Routines/ValidateGameDataRoutine.cs
@@ -1,4 +1,23 @@
-﻿using GameDevWare.Charon.Unity.Json;
+﻿/*
+	Copyright (c) 2023 Denis Zykov
+
+	This is part of "Charon: Game Data Editor" Unity Plugin.
+
+	Charon Game Data Editor Unity Plugin is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses.
+*/
+
+using GameDevWare.Charon.Unity.Json;
 using GameDevWare.Charon.Unity.Utils;
 using GameDevWare.Charon.Unity.Windows;
 using System.Collections.Generic;
@@ -7,7 +26,6 @@ using System;
 using System.Collections;
 using System.Diagnostics;
 using System.Linq;
-using System.Reflection;
 using GameDevWare.Charon.Unity.Async;
 using GameDevWare.Charon.Unity.ServerApi;
 using JetBrains.Annotations;
